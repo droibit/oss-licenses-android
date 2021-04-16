5268c441f3afbad2ec7426caa1cc42c8d811a029
Shinya Kumagai <roomful.rooms@gmail.com>
2021-04-16 14:53:19 +0900

    Update AGP and sample/libs dependencies

--- ui-wear/src/main/java/com/github/droibit/oss_licenses/ui/wearable/OssLicenseListFragment.kt
+++ ui-wear/src/main/java/com/github/droibit/oss_licenses/ui/wearable/OssLicenseListFragment.kt
@@ -4,83 +4,72 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.github.droibit.oss_licenses.parser.OssLicense
 import com.github.droibit.oss_licenses.parser.OssLicenseParser
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 private const val ARG_IGNORE_LIBRARIES = "ARG_IGNORE_LIBRARIES"
 
-class OssLicenseListFragment : Fragment(R.layout.fragment_oss_license_list),
-    CoroutineScope {
+internal class OssLicenseListFragment : Fragment(R.layout.fragment_oss_license_list) {
 
-  private val job = Job()
+    private val ossLicenses = MutableLiveData<List<OssLicense>>()
 
-  private val ossLicenses = MutableLiveData<List<OssLicense>>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-  override val coroutineContext: CoroutineContext
-    get() = Dispatchers.Default + job
+        val ignoreLibraries =
+            requireNotNull(requireArguments().getStringArrayList(ARG_IGNORE_LIBRARIES))
 
-  override fun onViewCreated(
-    view: View,
-    savedInstanceState: Bundle?
-  ) {
-    super.onViewCreated(view, savedInstanceState)
-
-    val adapter =
-      OssLicenseListAdapter(
-          requireContext()
-      ) {
-
-        parentFragmentManager.beginTransaction()
-            .setCustomAnimations(
-                androidx.fragment.R.anim.fragment_close_enter,
-                androidx.fragment.R.anim.fragment_open_exit,
-                androidx.fragment.R.anim.fragment_fade_enter,
-                androidx.fragment.R.anim.fragment_fade_exit
-            )
-            .replace(R.id.oss_licenses_content, OssLicenseFragment.newInstance(ossLicense = it))
-            .addToBackStack(null)
-            .commit()
-      }
-    view.findViewById<RecyclerView>(R.id.oss_licenses_list)
-        .also {
-          it.adapter = adapter
-          it.setHasFixedSize(true)
+        lifecycleScope.launch {
+            @Suppress("BlockingMethodInNonBlockingContext")
+            val parsedOssLicenses = OssLicenseParser.parse(requireContext(), ignoreLibraries)
+            ossLicenses.postValue(parsedOssLicenses)
         }
-    ossLicenses.observe(viewLifecycleOwner, Observer {
-      adapter.update(it)
-    })
-  }
-
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-
-    val ignoreLibraries =
-      requireNotNull(requireArguments().getStringArrayList(ARG_IGNORE_LIBRARIES))
-    launch {
-      @Suppress("BlockingMethodInNonBlockingContext")
-      val parsedOssLicenses = OssLicenseParser.parse(requireContext(), ignoreLibraries)
-      ossLicenses.postValue(parsedOssLicenses)
     }
-  }
 
-  override fun onDestroy() {
-    job.cancel()
-    super.onDestroy()
-  }
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
 
-  companion object {
+        val adapter =
+            OssLicenseListAdapter(
+                requireContext()
+            ) {
 
-    fun newInstance(ignoreLibraries: List<String>) = OssLicenseListFragment().apply {
-      arguments = Bundle(1).also {
-        it.putStringArrayList(ARG_IGNORE_LIBRARIES, ArrayList(ignoreLibraries))
-      }
+                parentFragmentManager.beginTransaction()
+                    .setCustomAnimations(
+                        androidx.fragment.R.animator.fragment_close_enter,
+                        androidx.fragment.R.animator.fragment_open_exit,
+                        androidx.fragment.R.animator.fragment_fade_enter,
+                        androidx.fragment.R.animator.fragment_fade_exit
+                    )
+                    .replace(
+                        R.id.oss_licenses_content,
+                        OssLicenseFragment.newInstance(ossLicense = it)
+                    )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        view.findViewById<RecyclerView>(R.id.oss_licenses_list)
+            .also {
+                it.adapter = adapter
+                it.setHasFixedSize(true)
+            }
+        ossLicenses.observe(viewLifecycleOwner) {
+            adapter.update(it)
+        }
     }
-  }
+
+    companion object {
+
+        fun newInstance(ignoreLibraries: List<String>) = OssLicenseListFragment().apply {
+            arguments = Bundle(1).also {
+                it.putStringArrayList(ARG_IGNORE_LIBRARIES, ArrayList(ignoreLibraries))
+            }
+        }
+    }
 }
