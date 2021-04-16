c316bffd7118d924eefc4ce4e1147beaba3e5841
Shinya Kumagai <roomful.rooms@gmail.com>
2021-04-16 15:04:00 +0900

    Migrate from Android Extensions to ViewBinding

--- sample-wear/src/main/java/com/github/droibit/oss_licenses/sample/MainActivity.kt
+++ sample-wear/src/main/java/com/github/droibit/oss_licenses/sample/MainActivity.kt
@@ -2,15 +2,19 @@ package com.github.droibit.oss_licenses.sample
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.github.droibit.oss_licenses.sample.databinding.ActivityMainBinding
 import com.github.droibit.oss_licenses.ui.wearable.WearableOssLicensesActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : FragmentActivity(R.layout.activity_main) {
+class MainActivity : FragmentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        show.setOnClickListener {
+        binding.show.setOnClickListener {
             val intent = WearableOssLicensesActivity.createIntent(
                 this,
                 listOf("com.google.errorprone:javac-shaded-9-dev-r4023-3")
