b6416c7a29548aadc2926788ea390a351f929e29
Shinya Kumagai <roomful.rooms@gmail.com>
2022-09-22 19:44:34 +0900

    Update sample app for Wear OS

--- sample-wear/src/main/java/com/github/droibit/oss_licenses/sample/MainActivity.kt
+++ sample-wear/src/main/java/com/github/droibit/oss_licenses/sample/MainActivity.kt
@@ -1,38 +1,81 @@
 package com.github.droibit.oss_licenses.sample
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
-import com.github.droibit.oss_licenses.sample.databinding.ActivityMainBinding
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.ScalingLazyColumn
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import com.github.droibit.oss_licenses.sample.theme.WearAppTheme
 import com.github.droibit.oss_licenses.ui.wear.WearableOssLicensesActivity
+import com.github.droibit.oss_licenses.ui.wear.compose.WearableOssLicensesActivity as WearableComposeOssLicensesActivity
+
+private val ignoreLibraries = setOf(
+  "kotlinx-coroutines-bom",
+  "Android Tracing",
+  "Animal Sniffer",
+  "Checker Framework Annotations",
+  "Error Prone",
+  "Guava JDK5",
+  "Guava JDK7",
+  "J2ObjC",
+  "JSR 305",
+  "JsInterop Annotations",
+  "apksig",
+  "JSpecify",
+)
 
 class MainActivity : FragmentActivity() {
-
-  private lateinit var binding: ActivityMainBinding
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-
-    binding.show.setOnClickListener {
-      val intent = WearableOssLicensesActivity.createIntent(
-        this,
-        setOf(
-          "kotlinx-coroutines-bom",
-          "Android Tracing",
-          "Animal Sniffer",
-          "Checker Framework Annotations",
-          "Error Prone",
-          "Guava JDK5",
-          "Guava JDK7",
-          "J2ObjC",
-          "JSR 305",
-          "JsInterop Annotations",
-          "apksig",
-          "JSpecify",
-        ),
-      )
-      startActivity(intent)
+    setContent {
+      WearAppTheme {
+        Scaffold(
+          timeText = {
+            TimeText()
+          },
+        ) {
+          ScalingLazyColumn(
+            modifier = Modifier.fillMaxSize(),
+          ) {
+            item {
+              Chip(
+                label = {
+                  Text(text = "Show")
+                },
+                onClick = {
+                  val intent = WearableOssLicensesActivity.createIntent(
+                    this@MainActivity,
+                    ignoreLibraries,
+                  )
+                  startActivity(intent)
+                },
+                modifier = Modifier.fillMaxWidth(),
+              )
+            }
+            item {
+              Chip(
+                label = {
+                  Text(text = "Show(Compose)")
+                },
+                onClick = {
+                  val intent = WearableComposeOssLicensesActivity.createIntent(
+                    this@MainActivity,
+                    ignoreLibraries,
+                  )
+                  startActivity(intent)
+                },
+                modifier = Modifier.fillMaxWidth(),
+              )
+            }
+          }
+        }
+      }
     }
   }
 }
