530413d279a24cffa6f892f3bef60dd2a24523de
Shinya Kumagai <roomful.rooms@gmail.com>
2025-01-21 15:29:32 +0900

    Refactor WearableOssLicensesActivity to remove unnecessary ViewModel usage

--- ui-wear-compose-material/src/main/java/com/github/droibit/oss_licenses/ui/wear/compose/material/WearableOssLicensesActivity.kt
+++ ui-wear-compose-material/src/main/java/com/github/droibit/oss_licenses/ui/wear/compose/material/WearableOssLicensesActivity.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.wear.compose.material.MaterialTheme
-import com.github.droibit.oss_licenses.ui.viewmodel.OssLicenseViewModel
 import com.github.droibit.oss_licenses.ui.viewmodel.OssLicenseViewModel.Companion.EXTRA_IGNORE_LIBRARIES
 import com.github.droibit.oss_licenses.ui.wear.compose.material.internal.OssLicenseNavGraph
 
@@ -15,20 +13,12 @@ import com.github.droibit.oss_licenses.ui.wear.compose.material.internal.OssLice
  * An activity that displays open source licenses.
  */
 class WearableOssLicensesActivity : ComponentActivity() {
-  private val viewModel: OssLicenseViewModel by viewModels()
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    if (savedInstanceState == null) {
-      viewModel.ensureLicenses()
-    }
-
     setContent {
       MaterialTheme {
-        OssLicenseNavGraph(
-          viewModel = viewModel,
-        )
+        OssLicenseNavGraph()
       }
     }
   }
