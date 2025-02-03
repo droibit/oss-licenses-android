feacf55b1c379ab0b74bd0f208909b7ecde7a64b
Shinya Kumagai <roomful.rooms@gmail.com>
2025-02-03 19:44:54 +0900

    Refactor Wear Material3 license detail screen
    - Migrate to `TransformingLazyColumn`
    - Adjust TransformingLazyColumn content padding to match list view
    - Split detail screen into separate composable components

--- ui-wear-compose-material3/src/main/java/com/github/droibit/oss_licenses/ui/wear/compose/material3/internal/detail/OssLicenseDetailScreen.kt
+++ ui-wear-compose-material3/src/main/java/com/github/droibit/oss_licenses/ui/wear/compose/material3/internal/detail/OssLicenseDetailScreen.kt
@@ -1,25 +1,17 @@
 package com.github.droibit.oss_licenses.ui.wear.compose.material3.internal.detail
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.wear.compose.material3.ListHeader
-import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.foundation.lazy.TransformingLazyColumnState
+import androidx.wear.compose.foundation.lazy.rememberTransformingLazyColumnState
 import androidx.wear.compose.material3.ScreenScaffold
-import androidx.wear.compose.material3.Text
 import com.github.droibit.oss_licenses.parser.OssLicense
-import com.github.droibit.oss_licenses.ui.wear.compose.core.OssLicenseDetail
 
 @Composable
 fun OssLicenseDetailScreen(
   license: OssLicense,
   modifier: Modifier = Modifier,
-  listState: LazyListState = rememberLazyListState(),
+  listState: TransformingLazyColumnState = rememberTransformingLazyColumnState(),
 ) {
   ScreenScaffold(
     scrollState = listState,
@@ -27,25 +19,8 @@ fun OssLicenseDetailScreen(
   ) {
     OssLicenseDetail(
       license = license,
-      header = {
-        ListHeader {
-          Text(
-            text = license.library,
-            textAlign = TextAlign.Center,
-            overflow = TextOverflow.Ellipsis,
-          )
-        }
-      },
-      content = { license ->
-        Text(
-          text = license.text,
-          modifier = Modifier.fillMaxWidth(),
-          style = MaterialTheme.typography.bodyMedium.copy(
-            fontFamily = FontFamily.Monospace,
-          ),
-        )
-      },
       listState = listState,
+      modifier = modifier,
     )
   }
 }
