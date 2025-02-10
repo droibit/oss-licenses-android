d2721a9a851aadbeaf212c33dbc19731f8380f36
Shinya Kumagai <roomful.rooms@gmail.com>
2025-02-11 00:00:53 +0900

    Configure ktlint v1.5.0 to minimize code style changes
    - Set ktlint_official as the base code style
    - Disable specific rules to preserve existing formatting:
      - multiline-expression-wrapping
      - class-signature
      - function-signature
    
    The changes minimize formatting differences when ktlint formatter is applied.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,12 @@ subprojects {
       ktlint(libs.versions.ktlint.get())
         .editorConfigOverride(
           mapOf(
+            "ktlint_code_style" to "ktlint_official",
             "ktlint_function_naming_ignore_when_annotated_with" to "Composable, Test",
             "ktlint_standard_package-name" to "disabled",
+            "ktlint_standard_multiline-expression-wrapping" to "disabled",
+            "ktlint_standard_class-signature" to "disabled",
+            "ktlint_standard_function-signature" to "disabled",
           ),
         )
         .customRuleSets(
