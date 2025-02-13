7a39238741d2434c3be5238928e24de694799195
Shinya Kumagai <roomful.rooms@gmail.com>
2025-02-13 20:09:12 +0900

    Add screenshot tests for OssLicensesPaneContent
    - Add test class for adaptive pane content layout
    - Test different device configurations:
      - Phone (light/dark/Japanese)
      - Tablet (normal/large fonts)
    - Generate initial reference screenshots
    - Include test setup for Material 3 adaptive navigation

---
+++ ui-compose-material3/src/test/java/com/github/droibit/oss_licenses/ui/compose/material3/internal/OssLicensesPaneContentScreenshotTest.kt
@@ -0,0 +1,40 @@
+package com.github.droibit.oss_licenses.ui.compose.material3.internal
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
+import androidx.compose.ui.Modifier
+import com.github.droibit.oss_licenses.parser.OssLicense
+import com.github.droibit.oss_licenses.ui.compose.screenshots.Device
+import com.github.droibit.oss_licenses.ui.compose.screenshots.ScreenshotTest
+import com.github.droibit.oss_licenses.ui.compose.screenshots.TestFixtures
+import org.junit.Test
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+class OssLicensesPaneContentScreenshotTest(device: Device) : ScreenshotTest(device) {
+  @Test
+  fun paneContent() = runScreenshotTest {
+    OssLicensesTheme {
+      val licenses = TestFixtures.licenses
+
+      val navigator = rememberListDetailPaneScaffoldNavigator<OssLicense>()
+      if (device in setOf(Device.PhoneDark, Device.Tablet, Device.TabletLargeFonts)) {
+        navigator.navigateTo(ListDetailPaneScaffoldRole.Detail, TestFixtures.kotlinCoroutines)
+      }
+
+      Scaffold(
+        topBar = {
+          OssLicensesTopAppBar(navigator)
+        },
+      ) { innerPadding ->
+        OssLicensesPaneContent(
+          licenses = licenses,
+          navigator = navigator,
+          modifier = Modifier.padding(innerPadding),
+        )
+      }
+    }
+  }
+}
