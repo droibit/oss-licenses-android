3d8fb4d59d780116a64d7907b15c0fe4d19776fd
Shinya Kumagai <roomful.rooms@gmail.com>
2025-02-16 17:53:37 +0900

    Add Maven Publish plugin for library publishing
    - Add vanniktech Maven Publish plugin (version 0.30.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   alias(libs.plugins.kotlin) apply false
   alias(libs.plugins.compose.compiler) apply false
   alias(libs.plugins.osslicenses) apply false
+  alias(libs.plugins.maven.publish) apply false
   alias(libs.plugins.spotless)
 }
 
